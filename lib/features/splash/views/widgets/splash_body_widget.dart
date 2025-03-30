ad7077343e2deac9300605e9123fdb37dc9a81cf
MaiiSmart <mai@smarttechno.co>
2025-03-30 02:26:11 +0200

    first

---
+++ lib/features/splash/views/widgets/splash_body_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashBodyView extends StatelessWidget {
+  const SplashBodyView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
