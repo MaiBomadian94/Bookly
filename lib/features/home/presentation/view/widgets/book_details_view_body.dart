9769ad2147ffcfa7cd3ef5e1cdcd4b342d3be806
Mai Bomadian <maibomadian@gmail.com>
2025-04-29 15:05:32 +0300

    make refactor on book details view

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,10 +1,7 @@
-import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/view/widgets/book_rating_widget.dart';
-import 'package:bookly_app/features/home/presentation/view/widgets/featured_list_view_item_image.dart';
-import 'package:bookly_app/features/home/presentation/view/widgets/similar_books_list_view.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'book_actions.dart';
+import 'books_details_section.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -12,8 +9,6 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-
     return CustomScrollView(
       slivers: [
         SliverFillRemaining(
@@ -23,54 +18,15 @@ class BookDetailsViewBody extends StatelessWidget {
             child: Column(
               children: [
                 const CustomBookDetailsAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * .17),
-                  child: const FeaturedListViewItemImage(),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 35.h, bottom: 6.h),
-                  child: Text(
-                    'The Jungle Book',
-                    style: Styles.textTitle30.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Rudyard Kipling',
-                  style: Styles.textTitle18.copyWith(
-                    fontWeight: FontWeight.w500,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.white.withOpacity(.7),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 18.h, bottom: 37.h),
-                  child: const BookRatingWidget(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                  ),
-                ),
-                const BookActions(),
+                const BookDetailsSection(),
                 Expanded(
                   child: SizedBox(
                     height: 50.h,
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 16.h),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'You can also like',
-                      style: Styles.textTitle14.copyWith(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ),
-                const SimilarBooksListView(),
-                const SizedBox(
-                  height: 30,
+                const SimilarBooksSection(),
+                SizedBox(
+                  height: 30.h,
                 ),
               ],
             ),
---
+++ lib/features/home/presentation/view/widgets/books_details_section.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../../core/utils/styles.dart';
+import 'book_actions.dart';
+import 'book_rating_widget.dart';
+import 'featured_list_view_item_image.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .17),
+          child: const FeaturedListViewItemImage(),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 35.h, bottom: 6.h),
+          child: Text(
+            'The Jungle Book',
+            style: Styles.textTitle30.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Text(
+          'Rudyard Kipling',
+          style: Styles.textTitle18.copyWith(
+            fontWeight: FontWeight.w500,
+            fontStyle: FontStyle.italic,
+            color: Colors.white.withOpacity(.7),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 18.h, bottom: 37.h),
+          child: const BookRatingWidget(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+        const BookActions(),
+      ],
+    );
+  }
+}
