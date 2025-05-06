0633de51f0833630ddc908d6ed29883e4c8bcb29
Mai Bomadian <maibomadian@gmail.com>
2025-05-06 13:29:14 +0300

    create api service

--- lib/features/home/data/repositories/home_repo.dart
+++ lib/features/home/data/repositories/home_repo.dart
@@ -1,6 +1,5 @@
 import 'package:bookly_app/features/home/data/models/book_model.dart';
 import 'package:dartz/dartz.dart';
-
 import '../../../../core/errors/failure.dart';
 
 abstract class HomeRepository {
